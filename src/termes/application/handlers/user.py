c2a1503c650a76900620253829ef771f5e777574
Respirens <thesergiyprotsanin@gmail.com>
2023-05-16 20:54:31 +0300

    Updated database/models.py

--- src/termes/application/handlers/user.py
+++ src/termes/application/handlers/user.py
@@ -13,7 +13,7 @@ router = APIRouter()
 @router.get("/{user_id}", response_model=schemas.UserResponse)
 async def get_by_id(user_id: int, database_session: Annotated[AsyncSession, Depends(get_database_session)]):
     try:
-        user: models.User = await database.get_user(user_id, database_session, include_profile=True)
+        user: models.User = await database.get_user(database_session, user_id, include_profile=True)
         if user.deleted:
             raise HTTPException(status_code=410, detail="User deleted")
         return schemas.UserResponse(user=schemas.User.from_orm(user))
--- src/termes/database/user.py
+++ src/termes/database/user.py
@@ -1,14 +1,15 @@
-from sqlalchemy import select
+from sqlalchemy import select, update, delete, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
+from .. import utils
 from ..errors import ItemNotFoundError
-from ..models import User
+from ..models import User, UserCredentials
 
 
 async def get_user(
-        user_id: int,
         database_session: AsyncSession,
+        user_id: int,
         *,
         include_profile: bool = False,
         include_credentials: bool = False,
@@ -28,3 +29,23 @@ async def get_user(
         raise ItemNotFoundError()
 
     return user
+
+
+async def delete_user(database_session: AsyncSession, user_id: int, delete_from_database: bool = False):
+    if delete_from_database:
+        statement = delete(User).where(User.id == user_id)
+    else:
+        statement = update(User).where(User.id == user_id).values(deleted=True)
+
+    await database_session.execute(statement)
+
+
+async def check_credentials(database_session: AsyncSession, email: str, password: str | None = None) -> bool:
+    statement = select(func.count()).select_from(UserCredentials).where(UserCredentials.email == email)
+
+    if password is not None:
+        statement.where(UserCredentials.hashed_password == utils.sha256(password))
+
+    count = (await database_session.execute(statement)).scalar()
+
+    return count > 0
