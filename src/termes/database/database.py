d4eec86adb7fac8d3f0804bda43dc4953369f871
Respirens <thesergiyprotsanin@gmail.com>
2023-05-19 17:02:17 +0300

    Created user registration

--- src/termes/database/database.py
+++ src/termes/database/database.py
@@ -1,4 +1,11 @@
+from enum import Enum, auto
+
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, AsyncSession
 
 engine: AsyncEngine | None = None
 sessionmaker: async_sessionmaker[AsyncSession] | None = None
+
+
+class FieldValue(Enum):
+    ANY = auto()
+    NOT_NONE = auto()
